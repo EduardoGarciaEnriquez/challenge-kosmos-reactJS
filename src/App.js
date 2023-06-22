f750ab82a0a1497c2d9f48fda7e7df62fb0b8cce
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-06-21 18:07:14 -0600

    deploy: gh-pages

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     const fetchData = async () => {
       const data = await fetch('https://jsonplaceholder.typicode.com/photos');
       const json = await data.json();
-      console.log(json)
+      // console.log(json)
     }
   
     fetchData()
